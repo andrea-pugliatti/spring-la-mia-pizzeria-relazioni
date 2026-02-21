e56e6d78f6d47b27e8b5e6e4ec35778d0aaba285
andrea-pugliatti <andrea.pugliatti@outlook.it>
2026-02-21 14:27:48 +0100

    add index view and add create and delete routes in IngredientController

--- src/main/java/org/lessons/java/spring_la_mia_pizzeria_relazioni/controllers/IngredientController.java
+++ src/main/java/org/lessons/java/spring_la_mia_pizzeria_relazioni/controllers/IngredientController.java
@@ -1,8 +1,20 @@
 package org.lessons.java.spring_la_mia_pizzeria_relazioni.controllers;
 
+import java.util.List;
+
+import org.lessons.java.spring_la_mia_pizzeria_relazioni.models.Ingredient;
 import org.lessons.java.spring_la_mia_pizzeria_relazioni.repositories.IngredientRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.validation.Valid;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @RequestMapping("/ingredients")
@@ -11,6 +23,39 @@ public class IngredientController {
 
     public IngredientController(IngredientRepository ingredientRepository) {
         this.ingredientRepository = ingredientRepository;
+    }
+
+    @GetMapping
+    public String getIndex(Model model) {
+        List<Ingredient> ingredients = ingredientRepository.findAll();
+
+        model.addAttribute("ingredients", ingredients);
+        model.addAttribute("new_ingredient", new Ingredient());
+
+        return "ingredients/index";
+    }
+
+    @PostMapping("/create")
+    public String postStore(
+            @Valid @ModelAttribute("new_ingredient") Ingredient ingredientForm,
+            BindingResult bindingResult,
+            Model model) {
+
+        if (!bindingResult.hasErrors()) {
+            ingredientRepository.save(ingredientForm);
+            model.addAttribute("new_ingredient", new Ingredient());
+        }
+
+        model.addAttribute("ingredients", ingredientRepository.findAll());
+
+        return "ingredients/index";
+    }
+
+    @PostMapping("/delete/{id}")
+    public String postDelete(Model model, @PathVariable("id") Integer ingredientId) {
+        ingredientRepository.deleteById(ingredientId);
+
+        return "redirect:/ingredients";
     }
 
 }
